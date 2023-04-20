8e6db4cb038d275a00c8c181dcb8470ececf04ae
たい <taisei@wsoft.ws>
2023-04-20 13:10:07 +0900

    Update service-worker.published.js

--- StudioSync/wwwroot/service-worker.published.js
+++ StudioSync/wwwroot/service-worker.published.js
@@ -1,6 +1,8 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
+self.addEventListener('fetch', () => { });
 
+/*
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
@@ -46,3 +48,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
+*/
