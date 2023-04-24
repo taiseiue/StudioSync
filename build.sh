6c3df933f55a87941237d362df7e0cc852f65773
taiseiue <taisei@wsoft.ws>
2023-04-25 02:51:12 +0900

    dotnet7にアップグレード

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet6
 ./dotnet6/dotnet workload install wasm-tools
 ./dotnet6/dotnet workload install wasm-tools-net6
 cd StudioSync
